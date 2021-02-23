48c67fd6b1dec526fd0eaac886778a2044de3aa9
filipkro <filip.kronstrom@gmail.com>
2021-02-23 12:58:13 +0100

    cleanup inception coral

--- classifiers/inception_coral.py
+++ classifiers/inception_coral.py
@@ -61,8 +61,8 @@ class Classifier_INCEPTION:
 
     def _inception_module(self, input_tensor, masked, stride=1, activation='linear'):
 
-        input_tensor = keras.layers.Lambda((lambda x: x))(input_tensor,
-                                                          mask=masked[:, :, 0])
+        # input_tensor = keras.layers.Lambda((lambda x: x))(input_tensor,
+        #                                                   mask=masked)
         if self.use_bottleneck and int(input_tensor.shape[-1]) > self.bottleneck_size:
             input_inception = keras.layers.Conv1D(filters=self.bottleneck_size,
                                                   kernel_size=1, padding='same',
@@ -77,32 +77,30 @@ class Classifier_INCEPTION:
         conv_l = []
 
         input_inception = keras.layers.Lambda((lambda x: x))(input_inception,
-                                                             mask=masked[:, :, 0])
+                                                             mask=masked)
 
         for i in range(len(kernel_size_s)):
             conv_l.append(keras.layers.Conv1D(filters=self.nb_filters,
                                               kernel_size=kernel_size_s[i],
                                               strides=stride, padding='same',
                                               activation=activation,
-                                              use_bias=False)(input_inception))
+                                              use_bias=True)(input_inception))
 
-        max_pool_1 = keras.layers.MaxPool1D(
-            pool_size=3, strides=stride, padding='same')(input_tensor)
+        max_pool_1 = keras.layers.MaxPool1D(pool_size=3, strides=stride,
+                                            padding='same')(input_inception)
 
         conv_6 = keras.layers.Conv1D(filters=self.nb_filters, kernel_size=1,
                                      padding='same', activation=activation,
-                                     use_bias=False)(max_pool_1)
-
-        conv_6 = keras.layers.Lambda((lambda x: x))(conv_6,
-                                                    mask=masked[:, :, 0])
+                                     use_bias=True)(max_pool_1)
 
         conv_l.append(conv_6)
 
         x = keras.layers.Concatenate(axis=2)(conv_l)
 
-        x = keras.layers.Lambda((lambda x: x))(x, mask=masked[:, :, 0])
+        x = keras.layers.Lambda((lambda x: x))(x, mask=masked)
         x = keras.layers.BatchNormalization()(x)
-        x = keras.layers.Activation(activation='relu')(x)
+        x = keras.layers.LeakyReLU()(x)
+        # x = keras.layers.Activation(activation='relu')(x)
         return x
 
     def _shortcut_layer(self, input_tensor, out_tensor):
@@ -112,7 +110,8 @@ class Classifier_INCEPTION:
         shortcut_y = keras.layers.BatchNormalization()(shortcut_y)
 
         x = keras.layers.Add()([shortcut_y, out_tensor])
-        x = keras.layers.Activation('relu')(x)
+        # x = keras.layers.Activation('relu')(x)
+        x = keras.layers.LeakyReLU()(x)
         return x
 
     def build_model(self, input_shape, nb_classes):
@@ -121,24 +120,26 @@ class Classifier_INCEPTION:
                                             name='mask')(input_layer)
         x = masked_layer
         input_res = masked_layer
+        mask = masked_layer[:,:,0]
 
         for d in range(self.depth):
 
-            x = self._inception_module(x, masked_layer)
+            x = self._inception_module(x, mask)
 
             if self.use_residual and d % 3 == 2:
                 input_res = keras.layers.Lambda((lambda x: x))(input_res,
-                                                               mask=masked_layer[:, :, 0])
+                                                               mask=mask)
                 x = self._shortcut_layer(input_res, x)
                 input_res = x
 
 
-        x = keras.layers.Dropout(0.2)(x)
+        # x = keras.layers.Dropout(0.2)(x)
         gap_layer = keras.layers.GlobalAveragePooling1D()(
-            x, mask=masked_layer[:, :, 0])
+            x, mask=mask)
 
         output_layer = keras.layers.Dense(self.nb_filters,
                                           name='result1')(gap_layer)
+        output_layer = keras.layers.LeakyReLU()(output_layer)
         output_layer = keras.layers.Dense(self.nb_filters, use_bias=False)(output_layer)
         output_layer = coral.CoralOrdinal(nb_classes)(output_layer)
 
